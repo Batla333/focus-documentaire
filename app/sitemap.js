0430e2fcc7216873e8914f61268a0702b9552d92
Remy <remy.labat444@gmail.com>
2025-10-14 13:39:42 +0200

    sitemap correction

--- app/sitemap.js
+++ app/sitemap.js
@@ -1,16 +1,30 @@
 export default function sitemap() {
+  const baseUrl = "https://focus-documentaire.vercel.app";
+
   return [
     {
-      url: "https://focus-documentaire.vercel.app",
+      url: `${baseUrl}/`,
       lastModified: new Date(),
       changeFrequency: "weekly",
-      priority: 1,
+      priority: 1.0,
     },
     {
-      url: "https://focus-documentaire.vercel.app/docutheque",
+      url: `${baseUrl}/docutheque`,
       lastModified: new Date(),
       changeFrequency: "monthly",
       priority: 0.8,
+    },
+    {
+      url: `${baseUrl}/articles`,
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.9,
+    },
+    {
+      url: `${baseUrl}/actualites`,
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.9,
     },
   ];
 }

1e91da6e2823e6a4beffb2444505ba80995afe6d
Remy <remy.labat444@gmail.com>
2025-10-14 14:34:06 +0200

    correct balises seo 2

--- app/docutheque/page.js
+++ app/docutheque/page.js
@@ -2,14 +2,7 @@
 import { useState } from "react";
 import Link from "next/link";
 
-export const metadata = {
-  title: "Docuthèque — FOCUS Documentaire",
-  description:
-    "Une sélection de films documentaires professionnels, étudiants et amateurs à découvrir dans la Docuthèque de FOCUS Documentaire.",
-openGraph: {
-    images: ["/images/logo.png"],
-  },
-  };
+
 export default function Docutheque() {
   const [filmActif, setFilmActif] = useState(null);
 
